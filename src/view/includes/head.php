99c32cd25b2e0754d65e19c3ea65c288a5ab4019
alberto <moraes_101@hotmail.com>
2020-10-09 15:19:18 -0300

    Ajuste no template

--- src/view/includes/head.php
+++ src/view/includes/head.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <link rel="stylesheet" href="./assets/vendor/nucleo/css/nucleo.css" type="text/css">
     <link rel="stylesheet" href="./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
-    <link rel="stylesheet" href="./assets/css/argon.css?v=1.2.0" type="text/css">
+    <link rel="stylesheet" href="./assets/css/argon.min.css?v=1.2.0" type="text/css">
     <link rel="stylesheet" href="./assets/css/style.css?v=1.2.0" type="text/css">
     <script type="text/javascript">
         const BASE_URL = '<?=base_url()?>';
