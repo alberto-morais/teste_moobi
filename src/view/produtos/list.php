418167501642e8649b27a04cd403e009f6fddb89
alberto <moraes_101@hotmail.com>
2020-10-18 01:23:56 -0300

    mutiple urls remote

--- src/view/produtos/list.php
+++ src/view/produtos/list.php
@@ -60,9 +60,6 @@
                                     <tr>
                                         <th scope="row">
                                             <div class="media align-items-center">
-                                                <a href="#" class="avatar rounded-circle mr-3">
-                                                    <img alt="Image placeholder" src="./assets/img/theme/bootstrap.jpg">
-                                                </a>
                                                 <div class="media-body">
                                                     <span class="name mb-0 text-sm"><?= $produto->nome ;?></span>
                                                 </div>
